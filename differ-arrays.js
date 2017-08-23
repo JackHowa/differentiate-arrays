6f32859f2e6b4f0203a6ca447442ad55046970f4
jackhowa <johncarverhoward@gmail.com>
2017-08-23 17:02:28 -0500

    Begin a working algorithm for checking difference with loops, compare

--- differ-arrays.js
+++ differ-arrays.js
@@ -1,6 +1,18 @@
 function diffArray(arr1, arr2) {
   var newArr = [];
-  // Same, same; but different.
+  var sortedArr1 = arr1.sort();
+  var sortedArr2 = arr2.sort();
+  for (i = 0; i < arr1.length; i++) {
+    if (sortedArr1[i] != sortedArr2[i]) {
+      if (newArr.includes(sortedArr1[i]) == false) {
+        newArr.push(sortedArr1[i]);
+      }
+      if (newArr.includes(sortedArr2[i]) == false) {
+        newArr.push(sortedArr2[i]);
+      }
+    }
+  }
+
   return newArr;
 }
 
