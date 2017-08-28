184995daf34a83b64f08ea852582a9ec0f439d28
jackhowa <johncarverhoward@gmail.com>
2017-08-28 10:15:38 -0500

    Add concat and filter function using includes comparator as checks

--- differ-arrays.js
+++ differ-arrays.js
@@ -1,27 +1,9 @@
 function diffArray(arr1, arr2) {
-  var newArr = [];
-  var sortedArr1 = arr1.sort();
-  var sortedArr2 = arr2.sort();
-  for (i = 0; i < arr1.length; i++) {
-    if (sortedArr1[i] != sortedArr2[i]) {
-      if (newArr.includes(sortedArr1[i]) == false && sortedArr1[i] != null) {
-        newArr.push(sortedArr1[i]);
-      }
-      else {
-        // if there's an includes
-        newArr = newArr.filter(function(element) {
-          return element === sortedArr1[i];
-        });
-
-
-      }
-      if (newArr.includes(sortedArr2[i]) == false && sortedArr2[i] != null) {
-        newArr.push(sortedArr2[i]);
-      }
-    }
-  }
-
-  return newArr;
+  return arr1
+    .concat(arr2)
+    .filter(
+        newArrItem => !arr1.includes(newArrItem) || !arr2.includes(newArrItem)
+    );
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
