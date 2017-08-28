3fea72a1836a930f7309e67f0978fe9742b79a6a
jackhowa <johncarverhoward@gmail.com>
2017-08-28 10:06:15 -0500

    Try adding the includes filter

--- differ-arrays.js
+++ differ-arrays.js
@@ -3,12 +3,17 @@ function diffArray(arr1, arr2) {
   var sortedArr1 = arr1.sort();
   var sortedArr2 = arr2.sort();
   for (i = 0; i < arr1.length; i++) {
-    // need to accomodate for not pushing similar but different lengths together
-    // diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
-    // -> ["diorite", "pink wool"]
     if (sortedArr1[i] != sortedArr2[i]) {
       if (newArr.includes(sortedArr1[i]) == false && sortedArr1[i] != null) {
         newArr.push(sortedArr1[i]);
+      }
+      else {
+        // if there's an includes
+        newArr = newArr.filter(function(element) {
+          return element === sortedArr1[i];
+        });
+
+
       }
       if (newArr.includes(sortedArr2[i]) == false && sortedArr2[i] != null) {
         newArr.push(sortedArr2[i]);
